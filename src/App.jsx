46ce27a81ac116740336351ce3e068884f3539c4
jrajan20 <jai_20j@hotmail.com>
2026-07-05 11:39:20 -0400

    loading Order changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Error from './ui/Error';
 import Menu,{loader as menuLoader} from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order,{loader as orderLoader}from './features/order/Order';
+import Order,{loader as orderLoader} from './features/order/Order';
 
 
 
@@ -36,7 +36,9 @@ const router = createBrowserRouter([
     path: '/order/:orderId',
     element: <Order />,
     loader: orderLoader,
-  }]
+    errorElement: <Error/>,
+  }
+  ]
 }
   
 ]);
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,49 +1,50 @@
 // Test ID: IIDSAT
-import getOrder from "../../services/apiRestaurant";
-
+import {getOrder} from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+// const order = {
+//   id: "ABCDEF",
+//   customer: "Jonas",
+//   phone: "123456789",
+//   address: "Arroios, Lisbon , Portugal",
+//   priority: true,
+//   estimatedDelivery: "2027-04-25T10:00:00",
+//   cart: [
+//     {
+//       pizzaId: 7,
+//       name: "Napoli",
+//       quantity: 3,
+//       unitPrice: 16,
+//       totalPrice: 48,
+//     },
+//     {
+//       pizzaId: 5,
+//       name: "Diavola",
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//     {
+//       pizzaId: 3,
+//       name: "Romana",
+//       quantity: 1,
+//       unitPrice: 15,
+//       totalPrice: 15,
+//     },
+//   ],
+//   position: "-9.000,38.000",
+//   orderPrice: 95,
+//   priorityPrice: 19,
+// };
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  const order = useLoaderData();
   const {
     id,
     status,
@@ -53,6 +54,7 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
+
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
@@ -83,10 +85,10 @@ function Order() {
     </div>
   );
 }
-export async function loader({ params }) {
+
+ export async function loader({ params }) {
   const orderId = params.orderId;
    const order = await getOrder(orderId);
   return order;
 } 
-
 export default Order;
