51026d28b865bdc5b89975f45005643f89964ab0
jrajan20 <jai_20j@hotmail.com>
2026-07-06 10:55:25 -0400

    tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+import path from "path";
+import checker from "vite-plugin-checker";
 
+const __dirname = path.resolve();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-})
+  plugins: [react(), 
+    checker({
+      eslint: {
+        // Points to your project's lint command
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,js,jsx}"', 
+         overrideConfigFile: path.resolve(__dirname, '.eslintrc.cjs'),
+      },
+    })
+  ],
+});
